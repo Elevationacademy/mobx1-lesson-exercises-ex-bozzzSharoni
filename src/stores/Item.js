86788c6c2bad1bb20f171698a466ceda81bf786d
Yonatan Sharoni <yonatan.sharoni@gmail.com>
2019-08-12 12:07:11 +0300

    finished ex2

--- src/stores/Item.js
+++ src/stores/Item.js
@@ -2,8 +2,10 @@ import { observable } from 'mobx'
 
 export class Item {
     @observable name
+    @observable location
     @observable completed = false
     constructor(name) {
         this.name = name
+        this.location="Super Sell"
     }
 }
